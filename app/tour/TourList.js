f2ce48b9acb520f38980cd40557a0ea07172f0cc
Safayet <hossainsafayet187@gmail.com>
2024-03-18 02:07:31 +0600

    tour change

--- app/tour/TourList.js
+++ app/tour/TourList.js
@@ -11,67 +11,7 @@ import t5 from "@/public/images/activities-14.jpg";
 const TourList = () => {
   return (
     <Fragment>
-      <Container className="topMargin">
-        <h1 className="text-center">Tour List</h1>
-        <Row className="mt-5">
-          <Col className="mb-5" lg={4} md={6} sm={12}>
-            <Card style={{ width: "23rem" }}>
-              <Image
-                className="tourCardImage"
-                height={100}
-                width={300}
-                alt="tour-list"
-                src={t1}
-              />
-              <Card.Body>
-                <Card.Title>Paragliding in Cox's Bazar Inani Beach</Card.Title>
-                <Card.Text>Starting from Tk 7000</Card.Text>
-                <Button variant="success">Details</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="mb-5" lg={4} md={6} sm={12}>
-            <Card style={{ width: "23rem" }}>
-              <Image className="tourCardImage" alt="tour-list" src={t2} />
-              <Card.Body>
-                <Card.Title>Paragliding in Cox's Bazar Inani Beach</Card.Title>
-                <Card.Text>Starting from Tk 7000</Card.Text>
-                <Button variant="success">Details</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="mb-5" lg={4} md={6} sm={12}>
-            <Card style={{ width: "23rem" }}>
-              <Image className="tourCardImage" alt="tour-list" src={t3} />
-              <Card.Body>
-                <Card.Title>Paragliding in Cox's Bazar Inani Beach</Card.Title>
-                <Card.Text>Starting from Tk 7000</Card.Text>
-                <Button variant="success">Details</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="mb-5" lg={4} md={6} sm={12}>
-            <Card style={{ width: "23rem" }}>
-              <Image className="tourCardImage" alt="tour-list" src={t4} />
-              <Card.Body>
-                <Card.Title>Paragliding in Cox's Bazar Inani Beach</Card.Title>
-                <Card.Text>Starting from Tk 7000</Card.Text>
-                <Button variant="success">Details</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="mb-5" lg={4} md={6} sm={12}>
-            <Card style={{ width: "23rem" }}>
-              <Image className="tourCardImage" alt="tour-list" src={t5} />
-              <Card.Body>
-                <Card.Title>Paragliding in Cox's Bazar Inani Beach</Card.Title>
-                <Card.Text>Starting from Tk 7000</Card.Text>
-                <Button variant="success">Details</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+     <h1>Hello</h1>
     </Fragment>
   );
 };
